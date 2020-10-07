6fc1e4da108656046554cc9909484b7c6bf8030c
John Rees <john@bitsushi.com>
2020-10-07 21:17:30 +0100

    header

--- www/src/components/app.js
+++ www/src/components/app.js
@@ -1,21 +1,20 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
-
-import Header from './header';
-
+import { Router } from "preact-router";
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Home from "../routes/home";
+import Profile from "../routes/profile";
+import Header from "./header";
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
-)
+  <div id="app">
+    <Header />
+    <section>
+      <Router>
+        <Home path="/" />
+        <Profile path="/profile/" user="me" />
+        <Profile path="/profile/:user" />
+      </Router>
+    </section>
+  </div>
+);
 
 export default App;
